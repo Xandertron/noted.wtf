add434adfabf7bddb067f51fa73ad277dc3e4a42
Xandertron <xandertron5000@gmail.com>
2022-11-04 11:12:45 -0500

    awdawd

--- pages/api/paste.js
+++ pages/api/paste.js
@@ -1,11 +1,12 @@
 import { server } from '../../config'
 const crypto = require("crypto")
+const { WebhookClient } = require("discord.js")
 let pastes = {
     "about": {
         content: "Hi, welcome to noted.wtf\n\nCredits:\nhttps://github.com/kognise/water.css\nhighlight.js\nnext.js"
     }
 }
-
+const log = new WebhookClient({ url: "https://discord.com/api/webhooks/1038121597074145310/yARH_7qWrLTy73ZG3oi-Ns4y9mc2QAL50mYqrlpRYPOMA5xOqxxFELwN3VOADvtqT8_B" });
 export default function handler(req, res) {
     try {
         if (req.method === "POST") {
@@ -28,13 +29,14 @@ export default function handler(req, res) {
                         content: req.body.content,
                         modifykey: modkey,
                         id: id,
-                        link: server+id
+                        link: server + id
                     }
                 }
+                log.send(`new paste created!\n${paste.link}`)
                 if (req.headers["content-type"] == "application/x-www-form-urlencoded") {
                     res.redirect(302, `${server}/${id}?key=${modkey}`)
                 }
-                else{
+                else {
                     res.status(200).json(paste)
                 }
             }
