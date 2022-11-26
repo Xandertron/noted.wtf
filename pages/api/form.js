ecfcc2dd27608187c28a2ba20ea28de8f644a7ca
Xandertron <xandertron5000@gmail.com>
2022-11-25 23:04:26 -0600

    recents and nextjs rewrite for raw

--- pages/api/form.js
+++ pages/api/form.js
@@ -8,6 +8,8 @@ const { WebhookClient } = require("discord.js")
 const log = new WebhookClient({ url: "https://discord.com/api/webhooks/1038121597074145310/yARH_7qWrLTy73ZG3oi-Ns4y9mc2QAL50mYqrlpRYPOMA5xOqxxFELwN3VOADvtqT8_B" });
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
+let recent = []
+
 export default async function handler(req, res) {
     try {
         if (req.method === "POST") {
@@ -32,18 +34,25 @@ export default async function handler(req, res) {
                     res.status(401).json({ error: "invalid paste key, please go back using your browser's back button" })
                 }
             }
-            let id = crypto.randomBytes(4).toString("base64url")
-            let modkey = crypto.randomBytes(10).toString("base64url")
             if (req.body.content.length >= 5) {
+                let id = crypto.randomBytes(4).toString("base64url")
+                let modkey = crypto.randomBytes(10).toString("base64url")
+                let expires = new Date(Date.now() + clamp(parse(req.body.expires), 30000, 2592000000))
                 const paste = await db.paste.create({
                     data: {
                         id: id,
-                        expiresAt: new Date(Date.now() + clamp(parse(req.body.expires), 30000, 2592000000)),
+                        expiresAt: expires,
                         content: req.body.content,
                         modifyKey: modkey
                     }
                 })
-                log.send(`new paste created!\n${server}/${id}`)
+                if (req.body.public) {
+                    if (recent.length >= 10) {
+                        recent.shift()
+                    }
+                    recent.push({ id: id, expires: expires })
+                }
+                log.send(`new ${req.body.public ? "public" : "private"} paste created!\n${server}/${id}`)
                 if (req.headers["content-type"] == "application/x-www-form-urlencoded") {
                     res.redirect(302, `${server}/${id}?key=${modkey}`)
                 }
@@ -53,6 +62,16 @@ export default async function handler(req, res) {
             }
             else {
                 res.status(400).json({ error: "content length must be more than 5 characters" })
+            }
+        }
+        else if(req.method=="GET"){
+            if (req.query.method === "recent") {
+                if (recent.length == 0) {
+                    res.status(404).json({ error: "no recent pastes" })
+                }
+                else {
+                    res.status(200).json(recent)
+                }
             }
         }
     }
